43e4922716d5eaa0cd01911f2fd324472dee9a92
analauracpaiva <analauracpaiva26@gmail.com>
2023-09-25 23:04:56 -0300

    c

--- src/components/AuthorsList.vue
+++ src/components/AuthorsList.vue
@@ -1,13 +1,25 @@
 <template>
   <div>
     <h2>Lista de Autores</h2>
-    
-    </div>
+    <ul>
+      <li v-for="author in authors" :key="author.id">{{ author.name }}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
+import Author from '@/models/Author'; 
+
 export default {
-}
+  data() {
+    return {
+      authors: [
+        new Author(1, 'Autor 1'),
+        new Author(2, 'Autor 2'),
+      ]
+    };
+  }
+};
 </script>
 
 
