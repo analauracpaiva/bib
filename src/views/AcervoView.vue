0b127f1485cece4d2f155ddb49f0b459fc44590e
analauracpaiva <analauracpaiva26@gmail.com>
2023-12-10 13:47:08 -0300

    finalizando

--- src/views/AcervoView.vue
+++ src/views/AcervoView.vue
@@ -15,16 +15,20 @@ const titulos = ref([])
 const isLoading = ref(false)
 
 const currentTitulo = reactive({
-  nome: '',
-  descricao: '',
-  alunos: [],
-  professor: ''
+    titulo: '',
+    categoria: '',
+    autor: '',
 })
 
 const currentAutor = reactive({
   nome: '',
   email: '',
 })
+
+const currentCategoria = reactive({
+  descricao: '',
+})
+
 
 function onFileChange(e) {
   file.value = e.target.files[0]
@@ -53,11 +57,9 @@ async function save() {
   currentTitulo.capa_attachment_key = image.attachment_key
   await TituloService.saveTitulo(currentTitulo)
   Object.assign(currentTitulo, {
-    nome: '',
-    descricao: '',
-    alunos: [],
-    professor: '',
-    capa_attachment_key: ''
+    titulo: '',
+    categoria: '',
+    autor: '',
   })
   const data = await TituloService.getAllTitulos()
   titulos.value = data
@@ -65,13 +67,24 @@ async function save() {
 }
 async function saveAutor() {
   isLoading.value = true
-  await AutorService.saveTitulo(currentAutor)
-  Object.assign(currentTitulo, {
+  await AutorService.saveAutor(currentAutor)
+  Object.assign(currentAutor, {
     nome: '',
     email: '',
   })
   const data = await AutorService.getAllAutores()
   autores.value = data
+  isLoading.value = false
+}
+
+async function saveCategoria() {
+  isLoading.value = true
+  await CategoriaService.saveCategoria(currentCategoria)
+  Object.assign(currentCategoria, {
+    descricao: '',
+  })
+  const data = await CategoriaService.getAllCategorias()
+  categorias.value = data
   isLoading.value = false
 }
 
@@ -91,30 +104,36 @@ function editTitulo(titulo) {
 <template>
   <Loading v-model:active="isLoading" is-full-page class="loading" />
   <div class="main">
-    <h1 class="Title">Livros em Nossa Biblioteca!</h1>
     <div class="bookform">
-      <form class="form" @submit.prevent="save">
-        <h3>Adicione um Livro</h3>
+      
+        
         <div class="form-col">
           <div class="input-col">
+            <form class="form" @submit.prevent="save">
+            <h3>Adicione um Livro</h3>
             <input v-model="currentTitulo.titulo" type="text" placeholder="Título" />
+            <p class="select-label">Autor:</p>
             <select v-model="currentTitulo.autor">
               <option
-                v-for="categoria in categorias"
-                :key="categoria.id"
-                v-bind:value="categoria.id"
+                v-for="autor in autores"
+                :key="autor.id"
+                v-bind:value="autor.id"
               >
-                {{ categoria.descricao }}
-              </option>
+                {{ autor.nome + " | " + autor.email }}
+              </option> 
             </select>
+            <p class="select-label">Categoria:</p>
             <select v-model="currentTitulo.categoria">
-              <option v-for="autor in autores" :key="autor.id" v-bind:value="autor.id">
-                {{ autor.nome }}
+              <option v-for="categoria in categorias" :key="categoria.id" v-bind:value="categoria.id">
+                {{ categoria.id + ") " + categoria.descricao }}
               </option>
             </select>
+            <button type="submit" class="button">Enviar</button>
+      </form>
           </div>
           <div class="input-col">
             <input
+              class="file-input"
               type="file"
               accept="image/png, image/jpeg"
               name="file_upload"
@@ -126,20 +145,31 @@ function editTitulo(titulo) {
               </div>
             </div>
           </div>
-          <div class="input-col autor">
+          <div class="input-col">
             <h3>Adicione um Autor</h3>
-            <input
+            <form @submit.prevent="saveAutor" class="form">
+              <input
               v-model="currentAutor.nome" placeholder="Nome"
             />
             <input
               v-model="currentAutor.email" placeholder="Email"
             />
+            <button type="submit" class="button">Enviar</button>
+            </form>
+          </div>
+          <div class="input-col">
+            <h3>Adicione uma Categoria</h3>
+            <form @submit.prevent="saveCategoria" class="form ">
+              <input
+              v-model="currentCategoria.descricao" placeholder="Descricao"
+            />
+            <button type="submit" class="button">Enviar</button>
+            </form>
             
           </div>
         </div>
-        <button type="submit" class="button">Enviar</button>
-      </form>
     </div>
+    <h1 class="Title">Livros no Acervo</h1>
     <div class="bookWeek">
       <div v-for="titulo in titulos" :key="titulo.id" class="bookBox">
         <img class="bookPic" v-bind:src="titulo.capa.file" alt="fotoLivro" />
@@ -154,6 +184,47 @@ function editTitulo(titulo) {
 </template>
 
 <style scoped>
+h3{
+  color: white;
+}
+input, select {
+  padding: 2%;
+}
+
+button{
+  padding: 2%;
+  background-color: #f89c3c;
+  margin-top: 10% !important;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover{
+  padding: 2%;
+  background-color: #c27c31;
+  transition: all 0.2s ease-in-out;
+  margin-top: 10% !important;
+  color: white;
+  border: none;
+  cursor: pointer;
+}
+
+.select-label{
+  margin-bottom: 1%;
+  color: white;
+}
+.file-input{
+  padding-left: 20%;
+}
+#preview{
+  margin-top: 5%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
 .autor{
   display: flex;
   align-items: normal !important;
@@ -162,13 +233,16 @@ function editTitulo(titulo) {
   display: flex;
   flex-direction: column;
   margin-right: 5%;
+  width: 25%;
+  padding-bottom: 2%;
 }
 .img {
   width: 100%;
   aspect-ratio: 6/8;
+
 }
 .cover {
-  width: 60%;
+  max-width: 60%;
   aspect-ratio: 6/8;
 }
 .form {
@@ -179,7 +253,10 @@ function editTitulo(titulo) {
 .form-col {
   display: flex;
   flex-direction: row;
-  align-items: center;
+  align-items: flex-start;
+  height: 100%;
+  justify-content: space-between; 
+
 }
 input {
   margin-top: 1%;
@@ -188,13 +265,14 @@ button {
   margin-top: 1%;
 }
 .bookform {
+  margin-top: 2%;
   padding: 1% 15%;
   background-color: #0b5394;
   margin-bottom: 2%;
 }
 .Title {
   margin-left: 15%;
-  margin-top: 5%;
+  margin-top: 2%;
 }
 .bookBox {
   width: 15%;
@@ -204,6 +282,7 @@ button {
   padding: 1%;
   background-color: #f1f1f1;
   border-radius: 1vh;
+  margin-bottom: 2%;
 }
 .hero {
   width: 100%;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="main">
         <div class="CardsView">
-            <h1>Notícias</h1>
             <div class="NoticiaCard">
                 <div class="NoticiaImgBox">
-                    <img class="NoticiaImg" src="alguma coisa">
+                    <img class="NoticiaImg" src="../assets/imgs/biblioteca-sa-corredor.jpg">
                 </div>
                 <div class="NoticiaText">
-                    <h1>Titulo</h1>
-                    <p class="text"> Lorem ipsum Argo et sum Dolor</p>
+                    <h1>Bem-vindo ao Tesouro do Conhecimento: Explore o Acervo da Biblioteca UFPR</h1>
+                    <p class="text"> Descubra um universo de sabedoria e cultura na Biblioteca da Universidade Federal do Paraná (UFPR). Nosso acervo é um testemunho vivo da riqueza intelectual, abrangendo diversas disciplinas e oferecendo uma fonte inesgotável de recursos para estudantes, pesquisadores e entusiastas do aprendizado.</p>
+                    <h2>Explorando o Acervo:</h2>
+                    <p>Em nosso site, você encontrará uma vasta coleção de obras que abrange desde clássicos atemporais até as mais recentes publicações acadêmicas. Navegue por categorias, explore coleções especiais e mergulhe em volumes que representam séculos de conhecimento acumulado.</p>
+                    <h2>Riqueza Cultural em Suas Mãos:</h2>
+                    <p>O acervo da Biblioteca UFPR vai além dos limites da educação formal. Oferecemos uma gama diversificada de recursos, incluindo obras raras, documentos históricos e exposições virtuais que proporcionam uma imersão única na riqueza cultural e intelectual.</p>
                 </div>
             </div>
               
@@ -18,13 +21,13 @@
 <style scoped>
 .NoticiaImg{
     width: 100%;
-    aspect-ratio: 4/3;
+    height: 100%;
 }
 .NoticiaImgBox{
-    width: 40%;
+    width: 50%;
 }
 .CardsView{
-    padding: 5% 15%;
+    padding: 5% 8%;
 }
 .NoticiaCard{
     width: 100%;
@@ -35,7 +38,7 @@
     margin: 2% 0%;
 }
 .NoticiaText{
-    min-width: 60%;
+    min-width: 50%;
     width: 60%;
     padding: 0 1%;
 }
